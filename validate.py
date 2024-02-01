61758a6e5e95082aa1d718646f8300c28f38e3e5
Alexander Wilde <alexander.wilde87@gmail.com>
2024-02-01 04:24:48 +0000

    Update validate script

--- validate.py
+++ validate.py
@@ -1,30 +1,12 @@
-import re
-import os
-import json
 import argparse
+import json
+import os
+from pathlib import Path
+from typing import Generator
 
-def json_value(value):
-  if type(value) is dict:
-    for a in json_obj(value):
-      yield a
-  elif type(value) is list:
-    for a in json_list(value):
-      yield a
 
-def json_obj(data):
-  for key, value in data.items():
-    if key == 'narration':
-      yield f"{value['id']}.mp3"
-    for a in json_value(value):
-      yield a
-
-def json_list(data):
-  for value in data:
-    for a in json_value(value):
-      yield a
-
-def parse_dv(file):
-  with open(file) as f:
+def parse_dv(file: Path) -> Generator[str, None, None]:
+  with file.open() as f:
     data = json.load(f)
 
   for campaign in data:
@@ -32,71 +14,92 @@ def parse_dv(file):
       for scene in scenario['scenes']:
         yield f'{campaign["id"]}.{scenario["id"]}.{scene["id"]}.mp3'
 
-def parse_arkham_cards(file):
-  with open(file) as f:
+
+def iter_arkham_cards_value(value) -> Generator[str, None, None]:
+  if type(value) is dict:
+    yield from iter_arkham_cards_obj(value)
+  elif type(value) is list:
+    yield from iter_arkham_cards_list(value)
+
+
+def iter_arkham_cards_obj(data) -> Generator[str, None, None]:
+  for key, value in data.items():
+    if key == 'narration':
+      yield f"{value['id']}.mp3"
+    yield from iter_arkham_cards_value(value)
+
+
+def iter_arkham_cards_list(data) -> Generator[str, None, None]:
+  for value in data:
+    yield from iter_arkham_cards_value(value)
+
+
+def parse_arkham_cards(file: Path) -> Generator[str, None, None]:
+  with file.open() as f:
     data = json.load(f)
-  return [
-    narration
-    for narration in json_value(data)
-  ]
 
-def list_arkham_cards_narration(path):
-  return [
-    narration
-    for root, _, files in os.walk(os.path.join(path, 'campaigns'))
-    for file in files
-    for narration in parse_arkham_cards(os.path.join(root, file))
-  ] + [
-    narration
-    for root, _, files in os.walk(os.path.join(path, 'return_campaigns'))
-    for file in files
-    for narration in parse_arkham_cards(os.path.join(root, file))
-  ]
+  yield from iter_arkham_cards_value(data)
 
-def list_audio(path):
-  return sorted([
-    audio
-    for root, dirs, files in os.walk(path)
-    for audio in files
-  ])
 
-def list_dissonant_voices_narration(path):
-  return [
-    narration
-    for narration in parse_dv(os.path.join(path, 'server', 'src', 'db.json'))
-  ]
+def list_arkham_cards_narration(path: Path) -> set[str]:
+  return {
+      narration
+      for root, _, files in os.walk(path / 'campaigns')
+      for file in files
+      if file.endswith('.json')
+      for narration in parse_arkham_cards(Path(root, file))
+  } | {
+      narration
+      for root, _, files in os.walk(path / 'return_campaigns')
+      for file in files
+      if file.endswith('.json')
+      for narration in parse_arkham_cards(Path(root, file))
+  }
 
-def is_valid_file(parser, arg):
-  if not os.path.isdir(arg):
+
+def list_audio(path: Path) -> set[str]:
+  return {
+      audio
+      for root, dirs, files in os.walk(path)
+      for audio in files
+      if audio.endswith('.mp3')
+  }
+
+
+def list_dissonant_voices_narration(path: Path) -> set[str]:
+  return {
+      narration
+      for narration in parse_dv(path / 'src' / 'server' / 'data.json')
+  }
+
+
+def is_valid_file(parser: argparse.ArgumentParser, arg: str) -> Path:
+  path = Path(arg)
+  if not path.is_dir():
     parser.error(f'The folder {arg} does not exist!')
-  else:
-    return arg
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Process some integers.')
+  return path
+
+
+def main():
+  parser = argparse.ArgumentParser()
   parser.add_argument(
-    '-n',
-    dest='narration',
-    required=True,
-    help='input file',
-    metavar='FILE',
-    type=lambda x: is_valid_file(parser, x),
+      dest='narration',
+      help='input file',
+      metavar='FILE',
+      type=lambda x: is_valid_file(parser, x),
   )
   parser.add_argument(
-    '-d',
-    dest='dv',
-    required=True,
-    help='input file',
-    metavar='FILE',
-    type=lambda x: is_valid_file(parser, x),
+      dest='dv',
+      help='input file',
+      metavar='FILE',
+      type=lambda x: is_valid_file(parser, x),
   )
   parser.add_argument(
-    '-a',
-    dest='audio',
-    required=True,
-    help='input file',
-    metavar='FILE',
-    type=lambda x: is_valid_file(parser, x),
+      dest='audio',
+      help='input file',
+      metavar='FILE',
+      type=lambda x: is_valid_file(parser, x),
   )
 
   args = parser.parse_args()
@@ -104,18 +107,24 @@ if __name__ == '__main__':
   dissonant_voices_narration = list_dissonant_voices_narration(args.dv)
   audios_files = list_audio(args.audio)
 
-  a = {
-    narration: {
-      'arkham_cards': narration in arkham_cards_narration,
-      'dissonant_voices': narration in dissonant_voices_narration,
-      'audio': narration in audios_files,
-    }
-    for narration in (arkham_cards_narration + dissonant_voices_narration + audios_files)
+  narration = {
+      filename: {
+          'arkham_cards': filename in arkham_cards_narration,
+          'dissonant_voices': filename in dissonant_voices_narration,
+          'audio': filename in audios_files,
+      }
+      for filename in sorted(arkham_cards_narration | dissonant_voices_narration | audios_files)
   }
-  for narration, exists in a.items():
+  for narration, exists in narration.items():
     if all(exists.values()):
+      continue
+    elif not exists['arkham_cards']:
       continue
 
     print(narration)
     for type, found in exists.items():
       print(f'  {type}: {"✅" if found else "❌"}')
+
+
+if __name__ == '__main__':
+  main()
