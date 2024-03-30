90013ba378cf41f3d31b85c9a751b656b193216e
North101 <alexander.wilde87@gmail.com>
2024-03-30 22:52:53 +0000

    Remove luxon Add @popperjs/core

--- src/server/services/jwt.ts
+++ src/server/services/jwt.ts
@@ -1,18 +1,15 @@
 import jwt from 'jwt-simple'
-import { DateTime } from 'luxon'
 import { Token } from 'simple-oauth2'
 import config from '../config'
 
 export interface JwtPayload {
   token: Token
-  issued: number
 }
 
 export default class JwtService {
   encodeToken(token: Token) {
     const payload: JwtPayload = {
       token,
-      issued: DateTime.utc().toMillis(),
     }
     return jwt.encode(payload, config.jwt.secret, config.jwt.algorithm)
   }
